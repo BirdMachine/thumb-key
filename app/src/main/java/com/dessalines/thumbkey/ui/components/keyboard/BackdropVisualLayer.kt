0d6b7cc54e8371832c00b957444ead44ca07a078
Bird <jeswaldrip@gmail.com>
2026-08-27 23:17:48 -0400

    Support backdrop media on Android 7+

--- app/src/main/java/com/dessalines/thumbkey/ui/components/keyboard/BackdropVisualLayer.kt
+++ app/src/main/java/com/dessalines/thumbkey/ui/components/keyboard/BackdropVisualLayer.kt
@@ -1,9 +1,13 @@
 package com.dessalines.thumbkey.ui.components.keyboard
 
+import android.content.Context
 import android.graphics.ImageDecoder
 import android.graphics.drawable.AnimatedImageDrawable
+import android.graphics.drawable.Drawable
 import android.net.Uri
+import android.os.Build
 import android.widget.ImageView
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
@@ -11,6 +15,20 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
+
+@RequiresApi(Build.VERSION_CODES.P)
+private fun decodeModernBackdrop(
+    context: Context,
+    uri: Uri,
+): Drawable {
+    val source = ImageDecoder.createSource(context.contentResolver, uri)
+    return ImageDecoder.decodeDrawable(source).also { drawable ->
+        if (drawable is AnimatedImageDrawable) {
+            drawable.repeatCount = AnimatedImageDrawable.REPEAT_INFINITE
+            drawable.start()
+        }
+    }
+}
 
 @Composable
 fun BackdropVisualLayer(
@@ -43,25 +61,21 @@ fun BackdropVisualLayer(
                 update = { imageView ->
                     if (uri == null) {
                         imageView.setImageDrawable(null)
-                    } else {
+                    } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
                         runCatching {
-                            val source = ImageDecoder.createSource(context.contentResolver, uri)
-                            val drawable = ImageDecoder.decodeDrawable(source)
+                            decodeModernBackdrop(context, uri)
+                        }.onSuccess { drawable ->
                             imageView.setImageDrawable(drawable)
-                            if (drawable is AnimatedImageDrawable) {
-                                drawable.repeatCount = AnimatedImageDrawable.REPEAT_INFINITE
-                                drawable.start()
-                            }
                         }.onFailure {
-                            imageView.setImageDrawable(null)
+                            imageView.setImageURI(uri)
                         }
+                    } else {
+                        imageView.setImageURI(uri)
                     }
                 },
             )
         }
 
-        BackdropMode.NONE -> {
-            Unit
-        }
+        BackdropMode.NONE -> {}
     }
 }
